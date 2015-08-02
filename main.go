34c9191e1e89b48ee6ff7845a8f2993ab685c6f9
--gloval <--gloval>
2015-08-02 19:49:59 -0400

    Created a go hello world and Docker file

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import "fmt"
-
+import "flag"
 
 func main() {
+	repoSiteCode := flag.String("repoSiteCode","gitlab", "possible values: gitlab (github in the future)")
+	destinationPath := flag.String("destination", ".", "clone to this location")
+
 	fmt.Println("Hello")
+	fmt.Printf("repoSiteCode:%s destinationPath:%s", repoSiteCode, destinationPath)
+
+	//TODO: get the html
+	//TODO: parse the repo links
+	//TODO: pull the repos to destination
+
 }
